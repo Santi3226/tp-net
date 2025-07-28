c098c93015cbca0768004decf10fe52f30702644
Santiago Dedich <tanti19091@hotmail.com>
2025-07-28 17:10:42 -0300

    CRUDs de entrega 1 completos, creado de gitignore provisional, correccion de dependencias, Forms pendientes

--- WinForms/WebAPI/Program.cs
+++ WinForms/WebAPI/Program.cs
@@ -1,11 +1,12 @@
 using Application.Services;
 using Domain.Model;
 using DTOs;
+using Microsoft.AspNetCore.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args); 
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); 
+builder.Services.AddSwaggerGen();
 builder.Services.AddHttpLogging(o => { });
 
 var app = builder.Build(); 
@@ -18,11 +19,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
-
-
-app.MapGet("/pacientes/{id}", (int id) =>
+ 
+app.MapGet("/Pacientes/{id}", (int id) =>
 {
     PacienteService pacienteService = new PacienteService();
 
@@ -37,14 +35,14 @@ app.MapGet("/pacientes/{id}", (int id) =>
 })
 .WithName("GetPaciente")
 .Produces<PacienteDTO>(StatusCodes.Status200OK)
-.Produces(StatusCodes.Status404NotFound)
-.WithOpenApi();
+.Produces(StatusCodes.Status404NotFound).
+WithOpenApi();
 
-app.MapGet("/pacientes", () =>
+app.MapGet("/Pacientes", () =>
 {
     PacienteService pacienteService = new PacienteService();
 
-    PacienteDTO dtos = pacienteService.GetAll();
+    var dtos = pacienteService.GetAll();
 
     return Results.Ok(dtos);
 })
@@ -52,7 +50,7 @@ app.MapGet("/pacientes", () =>
 .Produces<List<PacienteDTO>>(StatusCodes.Status200OK)
 .WithOpenApi();
 
-app.MapPost("/pacientes", (PacienteDTO dto) =>
+app.MapPost("/Pacientes", (PacienteDTO dto) =>
 {
     try
     {
@@ -76,9 +74,9 @@ app.MapPut("/pacientes", (PacienteDTO dto) =>
 {
     try
     {
-        PacienteService clienteService = new PacienteService();
+        PacienteService pacienteService = new PacienteService();
 
-        var found = clienteService.Update(dto);
+        var found = pacienteService.Update(dto);
 
         if (!found)
         {
@@ -116,6 +114,99 @@ app.MapDelete("/pacientes/{id}", (int id) =>
 .Produces(StatusCodes.Status404NotFound)
 .WithOpenApi();
 
+app.MapGet("/centrosAtencion/{id}", (int id) =>
+{
+    CentroAtencionService centroAtencionService = new CentroAtencionService();
+
+    CentroAtencionDTO dto = centroAtencionService.Get(id);
+
+    if (dto == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(dto);
+})
+.WithName("GetCentroAtencion")
+.Produces<CentroAtencionDTO>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status404NotFound).
+WithOpenApi();
+
+app.MapGet("/centrosAtencion", () =>
+{
+    CentroAtencionService centroAtencionService = new CentroAtencionService();
+
+    var dtos = centroAtencionService.GetAll();
+
+    return Results.Ok(dtos);
+})
+.WithName("GetAllCentrosAtencion")
+.Produces<List<CentroAtencionDTO>>(StatusCodes.Status200OK)
+.WithOpenApi();
+
+app.MapPost("/centrosAtencion", (CentroAtencionDTO dto) =>
+{
+    try
+    {
+        CentroAtencionService centroAtencionService = new CentroAtencionService();
+
+        CentroAtencionDTO centroAtencionDTO = centroAtencionService.Add(dto);
+
+        return Results.Created($"/centrosAtencion/{centroAtencionDTO.Id}", centroAtencionDTO);
+    }
+    catch (ArgumentException ex)
+    {
+        return Results.BadRequest(new { error = ex.Message });
+    }
+})
+.WithName("AddCentroAtencion")
+.Produces<CentroAtencionDTO>(StatusCodes.Status201Created)
+.Produces(StatusCodes.Status400BadRequest)
+.WithOpenApi();
+
+app.MapPut("/centrosAtencion", (CentroAtencionDTO dto) =>
+{
+    try
+    {
+        CentroAtencionService centroAtencionService = new CentroAtencionService();
+
+        var found = centroAtencionService.Update(dto);
+
+        if (!found)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.NoContent();
+    }
+    catch (ArgumentException ex)
+    {
+        return Results.BadRequest(new { error = ex.Message });
+    }
+})
+.WithName("UpdateCentroAtencion")
+.Produces(StatusCodes.Status404NotFound)
+.Produces(StatusCodes.Status400BadRequest)
+.WithOpenApi();
+
+app.MapDelete("/centrosAtencion/{id}", (int id) =>
+{
+    CentroAtencionService centroAtencionService = new CentroAtencionService();
+
+    var deleted = centroAtencionService.Delete(id);
+
+    if (!deleted)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.NoContent();
+
+})
+.WithName("DeleteCentroAtencion")
+.Produces(StatusCodes.Status204NoContent)
+.Produces(StatusCodes.Status404NotFound)
+.WithOpenApi();
 
 app.Run();
 
